e30ba4c79fc1304f92e4e5d2184fb03c75c0cb1d
Givitha2005 <115726408+Givitha2005@users.noreply.github.com>
2023-08-30 23:59:51 +0530

    URL-Shortner-FE

---
+++ url-shortner/src/config.js
@@ -0,0 +1,11 @@
+export const env = {
+    api : "http://localhost:4000/auth",
+    api1 : "http://localhost:4000/api",
+    redirect : "http://localhost:4000/api/shortUrl"
+
+    // api : "https://urlshortener-backend-sooty.vercel.app/auth",
+    // api1 : "https://urlshortener-backend-sooty.vercel.app/api",
+    // redirect : "https://urlshortener-backend-sooty.vercel.app/api/shortUrl"
+    
+    
+}
